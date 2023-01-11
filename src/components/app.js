f6666172b95bfa305277a99b7fa8daa02ef140c0
Per Ekskog <per.ekskog@gmail.com>
2023-01-11 14:54:49 +0100

    Read input message and append to message array.

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
-const messages = [
+import { useRef, useCallback } from "react";
+let messages = [
   { "id": 1, "sender": "left", "message": "a" },
   { "id": 2, "sender": "right", "message": "b" },
   { "id": 3, "sender": "left", "message": "b" }
@@ -10,9 +11,31 @@ const Message = (props) => (
   </div>
 )
 
+const MessageInputRef = (props) => {
+  const messageElement = useRef();
+
+  const formHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log("Submit:", messageElement.current?.value)
+      const l = messages.length;
+      messages = [...messages, { "id": l, "sender": props.sender, "message": messageElement.current?.value }]
+      console.log(messages);
+    })
+  return (
+    <form onSubmit={formHandler}>
+      <input ref={messageElement} type="text" />
+      <button type="submit">Send</button>
+    </form>
+  )
+}
+
 const App = () => (
   <>
-    {messages.map((m) => (<Message key={m.id} sender={m.sender} text={m.message} />))}
+    <div>
+      {messages.map((m) => (<Message key={m.id} sender={m.sender} text={m.message} />))}
+    </div>
+    <div><MessageInputRef sender="left" /></div>
   </>
 )
 
